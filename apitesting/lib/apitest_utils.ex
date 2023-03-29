f1f7f6297972faeb150dcc7c187ca1d91c6708b8
hpotdar21@gmail.com <hpotdar21@gmail.com>
2023-03-29 17:47:01 +0530

    Updated_commit

--- apitesting/lib/apitest_utils.ex
+++ apitesting/lib/apitest_utils.ex
@@ -1,8 +1,8 @@
-defmodule ApitestUtils do
- import HTTPoison
+defmodule   ApitestUtils do
+  import HTTPoison
 
- def doGet(url) do
-  {:ok, %HTTPoison.Response{body: body}} = HTTPoison.get(url)
-  body
- end
+  def doGet(url) do
+    {:ok, %HTTPoison.Response{body: body}} = HTTPoison.get(url)
+    body
+  end
 end
